28aaeaa2840a26d2a380db6b7cd4185cbb99f77e
malartsoft <malartsoft@gmail.com>
2017-05-10 18:04:30 +0700

    dđ

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Cho kien");
+
+
+
+
+
+    response.end("123");
 
 });
 
